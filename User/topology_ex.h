cda7fd7678c5566b0b4a95d11e3f0a640b8f01d6
Najibkassab <najeebkassab@hotmail.com>
2020-07-04 18:21:20 +0300

    it is release candidate

--- User/topology_ex.h
+++ User/topology_ex.h
@@ -17,21 +17,21 @@
 #include "stm32f0xx_hal.h"
 	 
 
-#define _N	2					// Number of array modules
+#define __N	2					// Number of array modules
 
 // Array modules
 #define _mod1	1<<3
 #define _mod2	2<<3
 
 // Topology
-static uint16_t array[_N][7] = {
+static uint16_t array[__N][7] = {
 { _H1BR6, 0, 0, 0, 0, _mod2|P5, 0},									// Module 1
 { _H07R3, 0, 0, 0, 0, _mod1|P5, 0},									// Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H1BR6	1
+	#define	H07R3	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
