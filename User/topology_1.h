1dd56da4b5bc41877e94245a7840ded101863329
Najibkassab <najeebkassab@hotmail.com>
2021-02-03 16:13:49 +0200

    serparte file and update year and Version to V0.2.4

--- User/topology_1.h
+++ User/topology_1.h
@@ -1,8 +1,8 @@
 /*
-    BitzOS (BOS) V0.0.0 - Copyright (C) 2016 Hexabitz
+    BitzOS (BOS) V0.2.4 - Copyright (C) 2021 Hexabitz
     All rights reserved
 
-    File Name     : topology_1.h
+    File Name     : topology_ex.h
     Description   : Array topology definition.
 */
 
@@ -17,22 +17,21 @@
 #include "stm32f0xx_hal.h"
 	 
 
-#define __N	2				// Number of array modules
+#define __N	2					// Number of array modules
 
 // Array modules
 #define _mod1	1<<3
 #define _mod2	2<<3
 
-
 // Topology
 static uint16_t array[__N][7] = {
-{ _H01R0, 0, 0, 0, _mod2|P5, 0, _mod3|P4},									// Module 1
-{ _H01R0, 0, 0, _mod4|P6, 0, _mod1|P4, 0},									// Module 2
+{ _H07R3, 0, 0, _mod2|P1, _mod1|P3,0 , 0},		// Module 3
+{ _H01R0,  _mod2|P3, 0, 0,0, 0, _mod1|P4},		// Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H0BR4	1
+	#define	H07R3	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
@@ -41,19 +40,20 @@ static uint16_t array[__N][7] = {
 	#define	_P6pol_normal	1
 #endif
 #if ( _module == 2 )
-	#define	H01R0	1
+	#define	H08R6	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
 	#define	_P4pol_normal	1	
-	#define	_P5pol_reversed	1
-	#define	_P6pol_normal	1
+	#define	_P5pol_normal	1
+	#define	_P6pol_reversed	1
 #endif
+
 
 #ifdef __cplusplus
 }
 #endif
-#endif /*__ topology_1_H */
+#endif /*__ topology_ex_H */
 
 
 /************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
