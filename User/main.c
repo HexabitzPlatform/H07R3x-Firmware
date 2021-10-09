d45ccda610c83615ddf72849977891d20a730cb4
Najib kassab <najeebkassab@hotmail.com>
2021-10-09 13:39:26 +0300

    Final Realse Candidate -->V0.2.5

--- User/main.c
+++ User/main.c
@@ -14,27 +14,22 @@
 
 uint32_t *waveAdd[MAX_WAVES] =  {
 	(uint32_t *) &waveByteCode_HiThere[0],
-	(uint32_t *) &waveByteCode_HowAreYou[0],
 };
 
 const uint32_t waveLength[MAX_WAVES] =  {
 	WAVEBYTECODE_HITHERE_LENGTH,
-	WAVEBYTECODE_HOWAREYOU_LENGTH,
 };
 
 const uint32_t waveResolution[MAX_WAVES] =  {
 	WAVEBYTECODE_HITHERE_BPS,
-	WAVEBYTECODE_HOWAREYOU_BPS,
 };
 
 const uint32_t waveRate[MAX_WAVES] =  {
 	WAVEBYTECODE_HITHERE_SPS,
-	WAVEBYTECODE_HOWAREYOU_SPS,
 };
 
 const char *waveName[MAX_WAVES] =  {		// Note wave name must be lower case
-	"hithere",
-	"howareyou",
+	"hithere"
 };
 
 /* Private function prototypes -----------------------------------------------*/
--- User/wave.h
+++ User/wave.h
@@ -5,23 +5,20 @@
 
 
 /* Maximum number of embedded waves */
-#define MAX_WAVES		2
+#define MAX_WAVES									1
 
 /* Length of embedded waves */
 #define WAVEBYTECODE_HOWAREYOU_LENGTH 				14202
-#define WAVEBYTECODE_HITHERE_LENGTH						9028
+#define WAVEBYTECODE_HITHERE_LENGTH					9028
 
 /* Resolution of embedded waves (bits per sample) */
-#define WAVEBYTECODE_HOWAREYOU_BPS 						8
-#define WAVEBYTECODE_HITHERE_BPS							8
+#define WAVEBYTECODE_HITHERE_BPS					8
 
 /* Rate of embedded waves (sample per second) */
-#define WAVEBYTECODE_HOWAREYOU_SPS 						16000
-#define WAVEBYTECODE_HITHERE_SPS							16000
+#define WAVEBYTECODE_HITHERE_SPS					16000
 
 /* Export embedded waves definition */
 extern const uint8_t waveByteCode_HiThere[WAVEBYTECODE_HITHERE_LENGTH];
-extern const uint8_t waveByteCode_HowAreYou[WAVEBYTECODE_HOWAREYOU_LENGTH];
 
 /* Export wave variables */
 extern uint32_t *waveAdd[MAX_WAVES];
